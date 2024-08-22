dc7fd08cdbffd76174a576f27d61e028e6600f84
karthik <karthikbncet@gmail.com>
2024-08-22 12:23:04 +0530

    Basic understanding of folder structure and adding new components.

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  standalone: true,
+  imports: [],
+  template: `<div>
+                Template
+            </div>`,
+  styles: [`div{
+    color:red
+  }`]
+})
+export class TestComponent {
+
+}
